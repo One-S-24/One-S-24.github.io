049c98c20af70c2b490fd2e7d1609f707eb1007a
One-S-24 <j241203j@gmail.com>
2024-11-08 09:44:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+    distant : '50px',
+    delay : 600,
+    duration : 1000,
+})
+
+sr.reveal('.img1', {origin: 'bottom', distance: '80px', 
+    duration: 1500, delay: 500})
+
+sr.reveal('.img2', {origin: 'top', distance: '80px', 
+    duration: 1500, delay: 700})
+
+sr.reveal('.img3', {origin: 'bottom', distance: '80px', 
+        duration: 1500, delay: 900})       
