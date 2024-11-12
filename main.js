a2ccefc326b892d64568ab8654a54ce97567548d
One-S-24 <j241203j@gmail.com>
2024-11-11 20:39:54 -0600

    Add files via upload
    Agregando login

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const sr = ScrollReveal({
     duration : 1000,
 })
 
+sr.reveal('.wrapper', {origin: 'bottom', distance: '80px', 
+    duration: 1400, delay: 400})
+    
 sr.reveal('.img1', {origin: 'bottom', distance: '80px', 
     duration: 1500, delay: 500})
 
@@ -12,3 +15,23 @@ sr.reveal('.img2', {origin: 'top', distance: '80px',
 
 sr.reveal('.img3', {origin: 'bottom', distance: '80px', 
         duration: 1500, delay: 900})       
+
+
+var wrapper = document.querySelector('.wrapper');
+var signuplink = document.querySelector('.signup-link');
+var loginlink = document.querySelector('.login-link');
+signuplink.onclick = () =>{
+    wrapper.classList.add("active");
+}
+loginlink.onclick = () =>{
+    wrapper.classList.remove("active");
+}
+
+
+function redirigirADashboard() {
+    window.location.href = "admin.html";
+  }
+  
+  function redirigirALogin() {
+    window.location.href = "admin.html"; 
+  }
